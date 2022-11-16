dc5ec35240649b4a2bcdacdfc24442d6f9c04ab3
TheReaLLsQuaLL <150316039@ogr.cbu.edu.tr>
2022-11-16 23:17:33 +0300

    -feat- Did the main look of the website no css yet and buttons does not work

--- src/App.js
+++ src/App.js
@@ -2,17 +2,21 @@ import logo from "./logo.svg";
 import "./App.css";
 import React from "react";
 import BreakInterval from "./components/BreakInterval";
+import SessionLength from "./components/SessionLength";
+import Timer from "./components/Timer";
 
 class App extends React.Component {
   constructor() {
     super();
-    this.state = { breakLength: 5, sessionLength: 25 };
+    this.state = { breakLength: 5, sessionLength: 25, timerMinute: 25 };
   }
   render() {
     return (
       <main>
         <h1>Pomodora Clock</h1>
         <BreakInterval breakInterval={this.state.breakLength} />
+        <SessionLength sessionLength={this.state.sessionLength} />
+        <Timer timerMinute={this.state.timerMinute} />
       </main>
     );
   }
---
+++ src/components/Timer.js
@@ -0,0 +1,28 @@
+import React from "react";
+
+class Timer extends React.Component {
+  constructor() {
+    super();
+    this.state = { isSession: true, timerSecond: 0 };
+  }
+  render() {
+    return (
+      <section>
+        <section>
+          <h3>{this.state.isSession === true ? "Session" : "Break"}</h3>
+          <span>{this.props.timerMinute}</span>
+          <span>:</span>
+          <span>
+            {this.state.timerSecond === 0
+              ? "00"
+              : this.state.timerMinute < 10
+              ? "0" + this.state.timerSecond
+              : this.state.timerSecond}
+          </span>
+        </section>
+      </section>
+    );
+  }
+}
+
+export default Timer;
